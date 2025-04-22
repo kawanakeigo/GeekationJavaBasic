2176ef6289782244db458c0eb64e1e272d5ea20b
kawanakeiware <kawanakeiware@localhost>
2025-04-23 00:47:49 +0900

    データ入力の課題を実行しました。ご確認よろしくお願いいたします。

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,20 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは");
+		System.out.println("私の名前は");
+		System.out.println("Geek君です。");
+		
+		System.out.println("こんにちは"+"私の名前は"+"Geek君です。");
+		
+		
+		System.out.println("1");
+		
+		
+
+	}
+
+}
