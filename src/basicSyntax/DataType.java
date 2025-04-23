ddcaa55f9b9f3fb392331e913bd2b9759b5479ee
kawanakeiware <kawanakeiware@localhost>
2025-04-23 18:44:18 +0900

    ご確認よろしくお願いいたします

--- src/basicSyntax/DataType.java
+++ src/basicSyntax/DataType.java
@@ -5,12 +5,9 @@ public class DataType {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println ("こんにちは"+"私の名前は"+"Geek君です。");
-		
 		System.out.println(10);
 		System.out.println("20"+"24");
-		
 		System.out.println(3.14);
-	
 		System.out.println(true);
 		
 	}
