08ab42fa89852cca0ca99ba1c6786dc355267d6d
kawanakeiware <kawanakeiware@localhost>
2025-04-24 06:40:09 +0900

    Variables.javaを作成して実装しました。確認よろしくお願いいたします。

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class Variables {
+
+public static void main(String[] args) {
+
+int num1 = 5 ;
+double num2 = 0.9 ;
+String suki ="私の好きな";
+String seisu ="整数は";
+String shousu ="少数は";
+String desu ="です。";
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(suki + seisu + num1 + desu);
+System.out.println(suki + shousu + num2 + desu);
+}
+}
