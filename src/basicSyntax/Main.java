f8e63c6ec6e2674bcb9dbc98d9fb32b0e2c377e9
kawanakeiware <kawanakeiware@localhost>
2025-05-07 09:31:05 +0900

    クラスの課題を完了しました。

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 175.5;
+
+        GeekIntroduction.greeting(age, height);
+
+        int num1 = 10;
+        int num2 = 50;
+
+        GeekIntroduction.specialSkill(num1, num2);
+    }
+}
