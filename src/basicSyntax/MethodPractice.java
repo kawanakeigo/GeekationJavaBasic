32eda6ab2d91d02866a705c68c15d142543903a0
kawanakeiware <kawanakeiware@localhost>
2025-05-08 01:08:20 +0900

    追加

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -28,7 +28,6 @@ public class MethodPractice {
     public static int multiply(int a, int b) {
         return a * b;
     }
-
     public static int divide(int a, int b) {
         return a / b;
     }
